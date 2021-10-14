88b429260d423672732b017acc860d6ff21f44bd
Paul Prenn <paul.j.prenn@gmail.com>
2021-10-14 12:47:27 +0200

    🚑 Integrated Maps

--- src/app/containers/Home/Home.tsx
+++ src/app/containers/Home/Home.tsx
@@ -1,18 +1,32 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Dimensions } from "react-native";
 import { View, Box, Text } from "native-base";
-type findProps = {
-  navigation: any;
-};
-const Home: React.FC<findProps> = ({ navigation }) => {
+import MapView from "react-native-maps";
+
+const Home = () => {
+  const [region, setRegion] = useState({
+    latitude: Number,
+    longitude: Number,
+    latitudeDelta: Number,
+    longitudeDelta: Number,
+  });
+
   return (
     <Box safeArea>
       <Text fontFamily="body" fontWeight={100} fontStyle="normal">
-        Home
+        <MapView style={homeStyles.map} />
+        <Box></Box>
       </Text>
     </Box>
   );
 };
-const homeStyles = StyleSheet.create({});
+const homeStyles = StyleSheet.create({
+  map: {
+    width: Dimensions.get("window").width,
+    height:
+      Dimensions.get("window").height -
+      (Dimensions.get("window").height / 100) * 40,
+  },
+});
 
 export default Home;
